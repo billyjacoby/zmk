892cdcc7adce645af52d6ff3a55bd58086f58483
Billy Jacoby <billyjacoby@gmail.com>
2022-12-27 10:58:20 -0500

    remove logo

--- app/boards/arm/nice_nano/custom_status_screen.c
+++ app/boards/arm/nice_nano/custom_status_screen.c
@@ -14,7 +14,7 @@
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-LV_IMG_DECLARE(zenlogo);
+// LV_IMG_DECLARE(zenlogo);
 LV_IMG_DECLARE(layers2);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
@@ -70,12 +70,12 @@ lv_obj_t *zmk_display_status_screen() {
     lv_img_set_src(LayersHeading, &layers2);
 #endif
 
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    lv_obj_t *zenlogo_icon;
-    zenlogo_icon = lv_img_create(screen, NULL);
-    lv_img_set_src(zenlogo_icon, &zenlogo);
-    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
-#endif
+// #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+//     lv_obj_t *zenlogo_icon;
+//     zenlogo_icon = lv_img_create(screen, NULL);
+//     lv_img_set_src(zenlogo_icon, &zenlogo);
+//     lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
+// #endif
 
     // lv_task_handler();
     lv_refr_now(NULL);
